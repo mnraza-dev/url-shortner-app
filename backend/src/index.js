b7be8c32070f0aae2ae83b3494c9b3a6ce469e0b
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-17 01:22:39 +0530

    refactor: clean up index.js and add empty files for user controller, auth middleware, user model, and user routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,23 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import {nanoid} from 'nanoid'
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+import { nanoid } from "nanoid";
+dotenv.config();
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.get('/api/create', (req, res) => {
-res.send(nanoid(7))
-})
+app.get("/api/create", (req, res) => {
+  const { url } = req.body;
+  console.log(url);
+
+  res.send(nanoid(7));
+});
+
+
+
+
+
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port http://localhost:${process.env.PORT}`)
-})
+  console.log(`Server is running on port http://localhost:${process.env.PORT}`);
+});
