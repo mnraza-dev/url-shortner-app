e18ab68faf3d8b01d10f437181482061e0c77203
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-17 01:16:50 +0530

    configure backend: set up .env file and update entry point in package.json

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,12 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const app = express()
+app.use(express.json())
+
+app.get('/api/create', (req, res) => {
+    
+})
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`)
+    console.log(`Server is running on port http://localhost:${process.env.PORT}`)
 })
